b047a43dcf46a61114c7f82dd32a16522e4f4e87
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-05-27 13:11:53 +0200

    explanation of simple test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,7 +3,19 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 test("renders learn react", () => {
+  // arrange
   render(<App />);
+
+  // act - when you check some functions
+  // in this test no functions there for ...nothing
+
+  // assert
+  /**
+   * there is three main types of methods of screen:
+   * -get function throw error if can not find given expression
+   * -find function return promis
+   * -query function ???
+   */
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });

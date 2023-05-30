959af2a45c5d4ad349a1eaa4e6dcc17a00c5bcae
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-05-30 21:11:15 +0200

    header change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,21 @@ import { useState } from "react";
 import MyPosts from "./MyPosts";
 
 const Header = () => {
-    return <header>
-        <h2>Nile Shop</h2>
+  const [show, setShow] = useState(false);
+
+  const toggleShow = () => {
+    setShow(!show);
+  };
+
+  return (
+    <header>
+      <h2>Nile Shop</h2>
+      {!show && <p>Not Clicked</p>}
+      {show && <div>Clicked</div>}
+      <button type="button" onClick={toggleShow}>
+        Click
+      </button>
+      <MyPosts />
     </header>
   );
 };

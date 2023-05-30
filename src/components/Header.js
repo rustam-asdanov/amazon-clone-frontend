17b55c6e9fd52b4b49d3e1a7d83a3cd6bdc56121
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-05-30 21:04:30 +0200

    some starter folders

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 const Header = () => {
-    return <header>
-        <h2>Nile Shop</h2>
+  return (
+    <header>
+      <h2>Nile Shop</h2>
+      <div>Search</div>
+      <div>Login/Registration</div>
+      <div>Cart</div>
     </header>
+  );
 };
 
 export default Header;

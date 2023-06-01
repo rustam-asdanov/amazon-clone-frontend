6a76281021b42bc2a40b2c3b9c5322c092d3c718
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-06-01 21:54:07 +0200

    header test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { useState } from "react";
 
 // imported folders
-import MyPosts from "./MyPosts";
 import ShopName from "./header/ShopName";
 import SearchBar from "./header/SearchBar";
 import Cart from "./header/Cart";
@@ -10,24 +8,12 @@ import SignIn from "./header/SignIn";
 import styles from "../styles/Header.module.css";
 
 const Header = (props) => {
-  const [show, setShow] = useState(false);
-
-  const toggleShow = () => {
-    setShow(!show);
-  };
-
   return (
     <nav className={`${props.class} ${styles.navbar}`}>
       <ShopName data-testid="shop-name" />
       <SearchBar data-testid="search-bar" />
       <SignIn data-testid="sign-in" />
       <Cart data-testid="cart" />
-      {/* {!show && <p>Not Clicked</p>}
-      {show && <div>Clicked</div>}
-      <button type="button" onClick={toggleShow}>
-        Click
-      </button>
-      <MyPosts /> */}
     </nav>
   );
 };
--- src/test/components/Header.test.js
+++ src/test/components/Header.test.js
@@ -1,62 +1,12 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import Header from "../../components/Header";
-import { act } from "react-dom/test-utils";
 
 describe("header component", () => {
-  test("render nile shop as a text", () => {
-    // Arrange
+  test("render all components", () => {
     render(<Header />);
-
-    // Act
-    // ...nothing
-
-    // Assert
-    const nileShop = screen.getByText(/nile shop/i);
-    expect(nileShop).toBeInTheDocument();
+    // Assert that the navigation component exists
+    const navElement = screen.getByRole("navigation");
+    expect(navElement).toBeInTheDocument();
   });
-
-  // test("render not clicked", () => {
-  //   // Arrange
-  //   render(<Header />);
-
-  //   // Act
-  //   const buttonElement = screen.getByRole("button");
-
-  //   // Assert
-  //   const notClicked = screen.getByText(/not clicked/i);
-  //   expect(notClicked).toBeInTheDocument();
-  // });
-
-  // test("render clicked", () => {
-  //   // Arrange
-  //   render(<Header />);
-
-  //   // Act
-  //   act(() => {
-  //     const buttonElement = screen.getByRole("button");
-  //     userEvent.click(buttonElement);
-  //   });
-
-  //   // Assert
-  //   const clickedText = screen.getByText(/clicked/i);
-  //   expect(clickedText).toBeInTheDocument();
-  //   expect(clickedText).toBeEnabled();
-  // });
-
-  // test("render not clicked", () => {
-  //   // Arrnage
-  //   render(<Header />);
-
-  //   // Act
-  //   act(() => {
-  //     const buttonElement = screen.getByRole("button");
-  //     userEvent.click(buttonElement);
-  //   });
-
-  //   // Assert
-  //   const notClickedText = screen.queryByText(/not clicked/i);
-  //   expect(notClickedText).toBeNull();
-  // });
 });

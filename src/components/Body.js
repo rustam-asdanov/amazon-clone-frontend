6e34a89e0cc2801287cac45435d4818adfeb92b8
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-06-15 15:07:30 +0200

    card added to body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Cart from "./body/Cart";
+import styles from "../styles/Body.module.css";
 
 const Body = () => {
   return (
-    <div>
+    <div className={styles["body"]}>
       <h1>I am Body</h1>
+      <nav>
+        <ul>
+          <li>laptop</li>
+          <li>Smartphone</li>
+        </ul>
+      </nav>
+      <section>
+        <Cart />
+      </section>
     </div>
   );
 };
---
+++ src/components/body/Cart.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../../styles/body/Cart.module.css";
+
+const Cart = () => {
+  return (
+    <div className={styles["cart"]}>
+      <img
+        src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-silver-gallery1-202206?wid=2000&hei=1537&fmt=jpeg&qlt=95&.v=1664558461707"
+        alt="mac-laptop"
+      />
+    </div>
+  );
+};
+
+export default Cart;

be691d6f40954aa1aa602ed6d58284d2c9e8677f
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-06-15 14:37:03 +0200

    icon of sign up changed

--- src/components/header/SignIn.js
+++ src/components/header/SignIn.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import account_icon from "../../assets/add-user.png";
+import styles from "../../styles/header/SignIn.module.css";
 
 const SignIn = () => {
-  return <Button variant="primary">Sign In</Button>;
+  return (
+    <Button variant="light">
+      <img
+        src={account_icon}
+        alt="account-icon"
+        className={styles["account-icon"]}
+      />
+    </Button>
+  );
 };
 
 export default SignIn;
